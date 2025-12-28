665cd3a7127a0c9c0bb13a27be789531953ac489
wahidbaksh737-lgtm <wahidbaksh737@gmail.com>
2025-12-28 18:02:59 +0500

    Create slider.js

---
+++ css/js/slider.js
@@ -0,0 +1,8 @@
+let slides = document.querySelectorAll('.slide');
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+}, 5000);
